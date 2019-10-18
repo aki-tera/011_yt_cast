36404fda16ef4cd2d9494ccb950bb8470d0802f5
aki-tera <aki.tera@gmail.com>
2019-10-18 20:57:37 +0900

    dev0.0:create develop

--- rasUSB/cgi-bin/yt_download.py
+++ rasUSB/cgi-bin/yt_download.py
@@ -12,18 +12,10 @@ cgitb.enable()
 #PythonのCGIスクリプトから出力したHTMLの日本語文字化け防止
 import sys
 import io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 
-form = cgi.FieldStorage()
-url = form.getvalue("submit")
 
-#出力ファイル名
-outtmpl = "%(title)s.%(ext)s"
-#出力フォルダ
-down_dir = "podcast/"
-ydl_opts = {"outtmpl": down_dir+outtmpl}
 
 #実際のダウンロード処理
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -50,3 +42,37 @@ html_body = """
 
 
 print(html_body)
+
+def main():
+    #PythonのCGIスクリプトから出力したHTMLの日本語文字化け防止
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+    #
+    form = cgi.FieldStorage()
+    url = form.getvalue("submit")
+
+    #出力ファイル名
+    outtmpl = "%(title)s.%(ext)s"
+    #出力フォルダ
+    down_dir = "podcast/"
+    ydl_opts = {"outtmpl": down_dir+outtmpl}
+
+    #podcastフォルダ内にファイルが多数あれば、削除する
+    check_file(folder_path, max_fail)
+
+    #rssから現在のitem数をカウントする
+    #戻り値はカウント数
+    item_counter = rss_checker(folder_path)
+
+    #対象のurlをダウンロードする
+    #ダウンロードするファイル名は、podcast000.mp4とする
+    #戻り値はタイトル、時間、サイズ
+    download_yt(folder_path, url)
+
+    #rssに新しいファイルを追加する
+    rss_modify(file_path, url_path, imege_path, file_title, file_time, file_size)
+
+    return
+
+if __name__ == "__main__":
+    main()
