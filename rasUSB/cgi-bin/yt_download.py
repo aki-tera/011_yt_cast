fc6d2afaed362565505cc7ac850028f77811650b
aki-tera <aki.tera@gmail.com>
2019-10-21 16:43:00 +0900

    dev0.1:Personal note(function call is at the end of the program)

--- rasUSB/cgi-bin/yt_download.py
+++ rasUSB/cgi-bin/yt_download.py
@@ -15,11 +15,9 @@ import io
 
 
 
-
-
 #実際のダウンロード処理
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#    ydl.download([url])
 
 
 
@@ -40,8 +38,30 @@ html_body = """
 </html>
 """
 
+html_body1 = """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Podcast Creater</title>
+<link rel='stylesheet' href='../CSS/style_cgi.css'>
+</head>
+<body>
+"""
 
-print(html_body)
+html_body2 = """
+<p>
+<input type='button' id='return_page' value='Return Top Page' onClick='document.location="../index.html";'>
+</body>
+</html>
+"""
+
+
+#print(html_body)
+
+def test(str):
+    print(str)
+
 
 def main():
     #PythonのCGIスクリプトから出力したHTMLの日本語文字化け防止
@@ -51,6 +71,7 @@ def main():
     form = cgi.FieldStorage()
     url = form.getvalue("submit")
 
+
     #出力ファイル名
     outtmpl = "%(title)s.%(ext)s"
     #出力フォルダ
@@ -58,21 +79,25 @@ def main():
     ydl_opts = {"outtmpl": down_dir+outtmpl}
 
     #podcastフォルダ内にファイルが多数あれば、削除する
-    check_file(folder_path, max_fail)
+    #check_file(folder_path, max_fail)
 
     #rssから現在のitem数をカウントする
     #戻り値はカウント数
-    item_counter = rss_checker(folder_path)
+    #item_counter = rss_checker(folder_path)
 
     #対象のurlをダウンロードする
     #ダウンロードするファイル名は、podcast000.mp4とする
     #戻り値はタイトル、時間、サイズ
-    download_yt(folder_path, url)
+    #download_yt(folder_path, url)
 
     #rssに新しいファイルを追加する
-    rss_modify(file_path, url_path, imege_path, file_title, file_time, file_size)
+    #rss_modify(file_path, url_path, imege_path, file_title, file_time, file_size)
+
+    test(html_body1+str(form)+str(url)+html_body2)
 
     return
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
+
+main()
