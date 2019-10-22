f38ae06986dac6ce6b57c51f987a21595de720cf
aki-tera <aki.tera@gmail.com>
2019-10-22 15:37:30 +0900

    dev0.2:add item number in movie.rss

--- rasUSB/cgi-bin/yt_download.py
+++ rasUSB/cgi-bin/yt_download.py
@@ -13,6 +13,8 @@ cgitb.enable()
 import sys
 import io
 
+#xmlの解析
+import xml.etree.ElementTree as ET
 
 
 #実際のダウンロード処理
@@ -62,18 +64,33 @@ html_body2 = """
 def test(str):
     print(str)
 
+def rss_checker(RC_folder_path):
+    tree = ET.parse(RC_folder_path)
+    root = tree.getroot()
+    counter = 0
+    for child in root:
+        for child1 in child:
+            for child2 in child1:
+                if child2.tag == "title":
+                    counter = counter + 1
+    return(str(counter).zfill(3))
+
+
+
+
 
 def main():
     #PythonのCGIスクリプトから出力したHTMLの日本語文字化け防止
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-    #
+    #postからurlを取得する
     form = cgi.FieldStorage()
     url = form.getvalue("submit")
 
-
+    #rssから現在のitem数をカウントする
+    #戻り値はカウント数
     #出力ファイル名
-    outtmpl = "%(title)s.%(ext)s"
+    outtmpl = "podcast"+rss_checker("podcast/movie.rss")+".mp4"
     #出力フォルダ
     down_dir = "podcast/"
     ydl_opts = {"outtmpl": down_dir+outtmpl}
@@ -81,9 +98,7 @@ def main():
     #podcastフォルダ内にファイルが多数あれば、削除する
     #check_file(folder_path, max_fail)
 
-    #rssから現在のitem数をカウントする
-    #戻り値はカウント数
-    #item_counter = rss_checker(folder_path)
+
 
     #対象のurlをダウンロードする
     #ダウンロードするファイル名は、podcast000.mp4とする
@@ -93,7 +108,7 @@ def main():
     #rssに新しいファイルを追加する
     #rss_modify(file_path, url_path, imege_path, file_title, file_time, file_size)
 
-    test(html_body1+str(form)+str(url)+html_body2)
+    test(html_body1+outtmpl+html_body2)
 
     return
 
