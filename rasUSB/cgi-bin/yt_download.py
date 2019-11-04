7043ed1740a835b4f1f4e97b7e81bf357ae8fb53
aki-tera <aki.tera@gmail.com>
2019-11-04 23:20:11 +0900

    dev1.6:amend Invalid indentation

--- rasUSB/cgi-bin/yt_download.py
+++ rasUSB/cgi-bin/yt_download.py
@@ -76,28 +76,28 @@ def rss_checker(RC_rss_path):
 
 
 def yt_download(YT_url, YT_ydl_opts, YT_down_dir):
-        """
-        指定のurlを指定のフォルダにダウンロードする
-        ファイル名等はオプション指示となる
-        Parameters
-        ----------
-        YT_url:string
-            url
-        YT_ydl_opts:dictionary
-            options for youtube_dl
-        YT_down_dir:string
-            download path
+    """
+    指定のurlを指定のフォルダにダウンロードする
+    ファイル名等はオプション指示となる
+    Parameters
+    ----------
+    YT_url:string
+        url
+    YT_ydl_opts:dictionary
+        options for youtube_dl
+    YT_down_dir:string
+        download path
 
-        Returns
-        ----------
-        counter:tuple
-            download file
-            title
-            description
-            file size
-            file type
-            time stamp
-        """
+    Returns
+    ----------
+    counter:tuple
+        download file
+        title
+        description
+        file size
+        file type
+        time stamp
+    """
     #実際のダウンロード処理
     with youtube_dl.YoutubeDL(YT_ydl_opts) as ydl:
         info_dict = ydl.extract_info(YT_url, download=False)
